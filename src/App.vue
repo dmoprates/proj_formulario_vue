4e9bd46badf6a76b96710206111ee799079fbbe6
dmoprates <prates.diego@gmail.com>
2024-02-15 09:58:49 -0300

    Commit 15/02/2024 - inclusão de código de acompanhamento do curso

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <formulario-view/>
+</template>
+
+<script>
+import FormularioView from './components/Formulario.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormularioView
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
